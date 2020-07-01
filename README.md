51bd697ca77228edc8c8f62263f29b25cde110d2
Tylar Murray <mail@tylar.info>
2020-07-01 14:15:34 -0400

    failed attempt to use uploader extension

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mbon_data_uploader
 frontend for uploading mbon data into the mbon-in-a-box stack
+
+
+```
+FLASK_APP=mbon_data_uploader
+```
--- mbon_data_uploader/__init__.py
+++ mbon_data_uploader/__init__.py
@@ -1,64 +1,23 @@
-import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
+# based on code from
+# https://www.google.com/search?client=firefox-b-1-d&sxsrf=ALeKk01rDxvso1gCGvfOIM3N23NWVU8WTQ%3A1593623338248&ei=KsP8Xq_VDoac_Qa4qJ-YCA&q=%22flask-uploads%22+tutorial&oq=%22flask-uploads%22+tutorial&gs_lcp=CgZwc3ktYWIQAzIECCMQJzoECAAQR1DsLVjWP2CoQWgAcAF4AIAB-AKIAdcMkgEHMC4yLjEuM5gBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwivku3HxazqAhUGTt8KHTjUB4MQ4dUDCAs&uact=5#kpvalbx=_RcP8Xq_yL8S1ggeX9pyYCg26
 
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, DATA
+
+UPLOADED_TIMESERIES_DEST = '/tmp/timeseries'
+DEBUG = True
 
 def create_app(test_config=None):
-
-    UPLOAD_FOLDER = '/path/to/the/uploads'
-    ALLOWED_EXTENSIONS = {'csv'}
-
     app = Flask(__name__)
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-    # if test_config is None:
-    #     # load the instance config, if it exists, when not testing
-    #     app.config.from_pyfile('config.py', silent=True)
-    # else:
-    #     # load the test config if passed in
-    #     app.config.from_mapping(test_config)
+    app.config.from_object(__name__)
 
-    # # ensure the instance folder exists
-    # try:
-    #     os.makedirs(app.instance_path)
-    # except OSError:
-    #     pass
+    uploaded = UploadSet('timeseries', DATA)
+    configure_uploads(app, uploaded)
 
-    # # a simple page that says hello
-    # @app.route('/hello')
-    # def hello():
-    #     return 'Hello, World!'
-
-    def allowed_file(filename):
-        return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-    @app.route('/', methods=['GET', 'POST'])
-    def upload_file():
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submit an empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return redirect(url_for('uploaded_file',
-                                        filename=filename))
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <input type=file name=file>
-          <input type=submit value=Upload>
-        </form>
-        '''
-
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload():
+        if request.method == 'POST' and 'photo' in request.files:
+            filename = photos.save(request.files['photo'])
+            return filename
+        return render_template('upload.html')
     return app
