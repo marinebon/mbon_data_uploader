88aa5f6f7fa231af4082262e85aabce32db64954
Tylar Murray <mail@tylar.info>
2020-07-01 17:14:32 -0400

    finish outline of features

--- mbon_data_uploader/__init__.py
+++ mbon_data_uploader/__init__.py
@@ -1,37 +1,40 @@
 import os
+import tempfile
+
+import pandas as pd
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 
 def create_app(test_config=None):
 
-    UPLOAD_FOLDER = '/path/to/the/uploads'
+    UPLOAD_FOLDER = tempfile.gettempdir()
     ALLOWED_EXTENSIONS = {'csv'}
 
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-    # if test_config is None:
-    #     # load the instance config, if it exists, when not testing
-    #     app.config.from_pyfile('config.py', silent=True)
-    # else:
-    #     # load the test config if passed in
-    #     app.config.from_mapping(test_config)
-
-    # # ensure the instance folder exists
-    # try:
-    #     os.makedirs(app.instance_path)
-    # except OSError:
-    #     pass
-
-    # # a simple page that says hello
-    # @app.route('/hello')
-    # def hello():
-    #     return 'Hello, World!'
 
     def allowed_file(filename):
         return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+    def handle_file(filepath):
+        assert csv in filepath
+        pd.read_csv(filepath)
+        # TODO: push csv into influxdb using
+        #   https://github.com/fabio-miranda/csv-to-influxdb/blob/master/csv-to-influxdb.py
+
+
+    @app.route('/upload_success', methods=['GET'])
+    def upload_success():
+        return '''
+        <!doctype html>
+        <title>File Uploaded</title>
+        <h1>Your file has been uploaded</h1>
+        '''
+
 
     @app.route('/', methods=['GET', 'POST'])
     def upload_file():
@@ -48,8 +51,10 @@ def create_app(test_config=None):
                 return redirect(request.url)
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return redirect(url_for('uploaded_file',
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                handle_file(filepath)
+                return redirect(url_for('upload_success',
                                         filename=filename))
         return '''
         <!doctype html>
