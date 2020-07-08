216eb43ad05b97074fa6c3d6a376532e141f9640
7yl4r <git@tylar.info>
2020-07-08 12:03:32 -0400

    finish influx insert fn

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mbon_data_uploader
 Frontend for uploading mbon data into the mbon-in-a-box stack.
-Flask app connects to influxdb and allows you to upload .csv files. 
+Flask app connects to influxdb and allows you to upload .csv files.
 
 
 
@@ -8,4 +8,16 @@ Flask app connects to influxdb and allows you to upload .csv files.
 ## test csv uploading
 ```bash
 export_csv_to_influx --csv /tmp/FKdbv2_Rrs_671_TS_VSNPP_daily_MIA_zeros.csv --dbname fwc_coral_disease --measurement demo --field_columns mean,climatology,anomaly --force_insert_even_csv_no_update True --server tylar-pc:8086 --time_column Time
+```
+
+## dealing with NA values
+The ExportCsvToInflux script inserts a value representing empty field values.
+By default the NA value representation is -999.
+I've tried modifying the source and following up with a `DELETE`, but in short:
+there isn't an easy way to fix this.
+The best we can do is filter out the NA values in our queries.
+It's easiest to use the grafana "text edit mode" for the query.
+The query should look something like this:
+```
+SELECT mean("anomaly") FROM "rs_chlora" WHERE "anomaly"!=-999 AND $timeFilter GROUP BY time($__interval)
 ```
--- mbon_data_uploader/handle_csv_file.py
+++ mbon_data_uploader/handle_csv_file.py
@@ -6,6 +6,9 @@ def handle_csv_file(filepath, form_args):
     """
     Pushes data from csv file into influxdb
     """
+    INFLUXDB_SERVER = "tylar-pc:8086"
+    DBNAME = "fwc_coral_disease"
+
     assert ".csv" in filepath
     measurement = form_args["measurement"]
     assert measurement is not None
@@ -13,13 +16,14 @@ def handle_csv_file(filepath, form_args):
     assert tag_set is not None
 
     # === insert tag columns & set proper na values
+    NA_REP = -999  # influxdb doesn't handle NA, NaN, null
     df = pd.read_csv(filepath)
     tag_columns = []
     for key_val in tag_set.split(','):
         key, val = key_val.split('=')
         df[key] = val
         tag_columns.append(key)
-    df.to_csv(filepath, na_rep="")  # TODO: influxdb expects ??? for na or NaN
+    df.to_csv(filepath, na_rep=NA_REP)
 
     # === submit to influxdb server
     # measurement,tag_set      field_set
@@ -27,11 +31,22 @@ def handle_csv_file(filepath, form_args):
     subprocess.run([
         "export_csv_to_influx",
         "--csv", filepath,
-        "--dbname", "fwc_coral_disease",
+        "--dbname", DBNAME,
         "--measurement", measurement,
         "--field_columns", "mean,climatology,anomaly",
         "--tag_columns", ','.join(tag_columns),
         "--force_insert_even_csv_no_update", "True",
-        "--server", "tylar-pc:8086",  # TODO: update this for prod
+        "--server", INFLUXDB_SERVER,  # TODO: update this for prod
         "--time_column", "Time"
     ], check=True)
+
+    # === TODO: remove "na_rep" values from influx that were inserted by
+    #           export_csv_to_influx
+    # subprocess.run([
+    #     "curl",
+    #     "-G",
+    #     INFLUXDB_SERVER + "/query",
+    #     "--data-urlencode",
+    #     "q=DELETE FROM " + DBNAME + " WHERE " + measurement + "==" + NA_REP,
+    #     # "-u", "influx_user:influx_user_pw"
+    # ], check=True)
