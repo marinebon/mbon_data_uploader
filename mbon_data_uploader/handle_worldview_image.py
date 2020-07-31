998b339b25e1a8ed8d1920466c939b5908c5c683
7yl4r <git@tylar.info>
2020-07-31 15:21:29 -0400

    1st real attempt at handling wv img

--- mbon_data_uploader/handle_worldview_image.py
+++ mbon_data_uploader/handle_worldview_image.py
@@ -1,6 +1,8 @@
 import os
 import shutil
+import subprocess
 
+from filepanther.metadata_to_filepath import filepath_to_metadata
 import psycopg2
 
 
@@ -26,23 +28,45 @@ def handle_worldview_image(filepath, form_args):
     curs = conn.cursor()
 
     assert ".tif" in filepath
+
     product_type = form_args["product_type"]
     assert product_type is not None
     region_name = form_args["region_name"]
     assert region_name is not None
 
-    # === insert file metadata into database
+    ()
+
     # format filepath using result from
-    # SELECT path_format_str FROM files_schema.products
-    #     WHERE short_name=={product_type};
-    filepath = "TODO"
+    curs.execute(
+        f"""
+        SELECT path_format_str FROM {SCHEMA_NAME}.products
+        WHERE short_name == {product_type};
+        """
+    )
+    product_format_str = curs.fetchone()[0]
 
-    multihash = get_hash()
+    # === collect metadata from filepath
+    metadata_dict = filepath_to_metadata(
+        product_format_str, filepath
+    )
+    # === combine with metadata from the form
+    metadata_dict.update({  # do checks for unequal vals instead of update
+        "product_short_name": product_type,
+        "area_short_name": region_name,
+        "multihash": _get_base58_multihash(filepath),
+        "n_bytes": os.path.getsize(filepath),
+    })
 
-    datetime = get_date_from_fpath()  # TODO
+    # === figure out new filepath where the file will be stored
+    fname = os.path.basename(filepath)
+    new_filepath = (
+        f"/srv/imars-objects/{metadata_dict['product_short_name']}/"
+        f"{metadata_dict['area_short_name']}/"
+        f"{fname}"
+    )
+    metadata_dict["filepath"] = new_filepath
 
-    n_bytes = get_file_size()  # TODO
-
+    # === insert file metadata into database
     curs.execute(
         f"""
         INSERT INTO {SCHEMA_NAME}.{TABLE_NAME}
@@ -52,14 +76,50 @@ def handle_worldview_image(filepath, form_args):
         )
         VALUES
         (
-            '{filepath}','{multihash}','web_submit',{datetime},0,
-            {n_bytes},'{product_type}','{region_name}','new'
+            '{metadata_dict['filepath']}',
+            '{metadata_dict['multihash']}',
+            'web_submit',
+            {metadata_dict['datetime']},0,
+            {metadata_dict['n_bytes']},
+            '{metadata_dict['product_short_name']}',
+            '{metadata_dict['area_short_name']}',
+            'new'
         );
         """
     )
 
     # === put file into filesystem imars-objects
-    fname = os.path.basename(filepath)
-    new_filepath = f"/srv/imars-objects/{region_name}/{product_type}/{fname}"
     # cp filepath new_filepath
     shutil.move(filepath, new_filepath)
+
+    # finalize db connection
+    conn.commit()
+    conn.close()
+
+
+def _get_base58_multihash(filepath):
+    # === FAILED attempt to create ipfs-style multihash:
+    # # TODO: could better handle big files see
+    # # https://www.quickprogrammingtips.com/python/
+    # #     how-to-calculate-sha256-hash-of-a-file-in-python.html
+    # with open(filepath, "rb") as fileobj:
+    #     bytes = fileobj.read()
+    #     hash = hashlib.sha256(bytes).hexdigest()  # raw bytes
+    #     multihash = "1220" + hash
+    #     return b58_multihash = base58.b58encode(bytes.fromhex(multihash))
+
+    # so let's just use ipfs:
+    _gethash_ipfs(filepath)
+
+
+def _gethash_ipfs(filepath):
+    """
+    Get hash using IPFS system call.
+    IPFS must be installed for this to work.
+    """
+    return subprocess.check_output([
+        'ipfs', 'add',
+        '-Q',  # --quieter    bool - Write only final hash
+        '-n',  # --only-hash  bool - Only chunk and hash; do not write to disk
+        filepath
+    ]).strip().decode('utf-8')
