b41d8b185f1dedcbe0d6020795943525f45a632b
Tylar <mail@tylar.info>
2022-09-09 19:20:25 -0400

    try adding log capture

--- mbon_data_uploader/handle_csv_file.py
+++ mbon_data_uploader/handle_csv_file.py
@@ -7,11 +7,28 @@ from mbon_data_uploader.strfy_subproc_error import subproc_error_wrapper
 
 def handle_csv_file(filepath, form_args):
     """
-    Pushes data from csv file into influxdb
+    Pushes data from csv file into influxdb.
+    Returns an html-formatted log file.
     """
     INFLUXDB_SERVER = os.environ["INFLUXDB_HOSTNAME"]
     DBNAME = "fwc_coral_disease"
 
+    # === log setup
+    log_stream = StringIO()
+    logger = logging.getLogger("{}.{}".format(
+        __name__,
+        sys._getframe().f_code.co_name)
+    )
+    stream_handler = logging.StreamHandler(log_stream)
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(
+        logging.Formatter(
+           '<pre>%(asctime)s|%(levelname)s\t|%(filename)s:%(lineno)s\t|%(message)s</pre>'
+        )
+    )
+    logger.addHandler(stream_handler)
+
+    # === basic sanity checks
     assert ".csv" in filepath
     measurement = form_args["measurement"]
     assert measurement is not None
@@ -33,12 +50,12 @@ def handle_csv_file(filepath, form_args):
     df.to_csv(filepath, na_rep=NA_REP)
 
     # === submit to influxdb server
-    logging.info("loading {}'s fields ({}) with tags={}".format(
+    logger.info("loading {}'s fields ({}) with tags={}".format(
         measurement, fields, tag_columns
     ))
     # measurement,tag_set      field_set
     # rs_oc_chlor,location=MIA anom,mean,clim
-    subproc_error_wrapper([
+    cmd_list = [
         "export_csv_to_influx",
         "--csv", filepath,
         "--dbname", DBNAME,
@@ -48,7 +65,11 @@ def handle_csv_file(filepath, form_args):
         "--force_insert_even_csv_no_update", "True",
         "--server", INFLUXDB_SERVER,
         "--time_column", time_column
-    ])
+    ]
+    logger.info(cmd_list)
+    subproc_error_wrapper(cmd_list)
+    
+    return log_stream.getvalue()
 
     # === TODO: remove "na_rep" values from influx that were inserted by
     #           export_csv_to_influx
